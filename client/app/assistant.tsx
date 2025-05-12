2ddb3e88b7277a5b7bcab7deafe130077e4f17b0
OMARALSURAIA1 <omar209999@gmail.com>
2025-05-12 14:10:36 +0300

    Fixing Issue26 And @7

--- client/app/assistant.tsx
+++ client/app/assistant.tsx
@@ -43,3 +43,4 @@ export const Assistant = () => {
     </AssistantRuntimeProvider>
   );
 };
+//
--- client/components/app-sidebar.tsx
+++ client/components/app-sidebar.tsx
@@ -69,3 +69,5 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
     </Sidebar>
   )
 }
+
+//
